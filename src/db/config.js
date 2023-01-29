0b5c386913dff187dd3456dfcdc6094bfaaf3358
atanu <atanuj383@gmail.com>
2023-01-29 19:44:34 +0530

    add

--- src/db/config.js
+++ src/db/config.js
@@ -1,11 +1,17 @@
-const mongoose =require('mongoose')
+const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/reactcrud',{
-    useCreateIndex:true,
-    useNewUrlParser:true,
-    useUnifiedTopology:true
-}).then(()=>{
-    console.log("connection done")
-}).catch((e)=>{
-    console.log("something error")
-})
+mongoose
+  .connect(
+    'mongodb+srv://atanumongo:atanumongo@firstmongoapp.dgqpo.mongodb.net/online_netlify?retryWrites=true&w=majority',
+    {
+      useCreateIndex: true,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => {
+    console.log('connection done');
+  })
+  .catch((e) => {
+    console.log('something error');
+  });
